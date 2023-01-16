65fb889a1dbc905fbc0f24c359b15272a15e52a3
Nolan Y? <ndymario@gmail.com>
2023-01-16 16:35:30 -0600

    Update validation to look nicer

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -16,14 +16,11 @@
         <form use:form>
             <h1>Login</h1>
 
-            <input type="email" name="email" use:validators={[required, email]} />
-            <HintGroup for="email">
-                <Hint on="required">This is a mandatory field</Hint>
-                <Hint on="email" hideWhenRequired>Email is not valid</Hint>
-            </HintGroup>
+            <label for="email">Email Address</label>
+            <input type="email" id="email" name="email" placeholder="Email address" use:validators={[required, email]} />
 
-            <input type="password" name="password" use:validators={[required]} />
-            <Hint for="password" on="required">This is a mandatory field</Hint>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Password" use:validators={[required]} />
 
             <button disabled={!$form.valid}>Login</button>
         </form>
