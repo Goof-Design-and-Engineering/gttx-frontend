28440b3ecd6bb8a0e2ee9535ad0dee7320a8e68a
Nolan Y? <ndymario@gmail.com>
2023-01-16 16:19:29 -0600

    Add PicoCSS to Login Page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,20 +4,31 @@
     const form = useForm();
 </script>
 
-<form use:form>
-    <h1>Login</h1>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+    <title>Hello, world!</title>
+</head>
 
-    <input type="email" name="email" use:validators={[required, email]} />
-    <HintGroup for="email">
-        <Hint on="required">This is a mandatory field</Hint>
-        <Hint on="email" hideWhenRequired>Email is not valid</Hint>
-    </HintGroup>
+<body>
+    <main class = "container">
+        <form use:form>
+            <h1>Login</h1>
 
-    <input type="password" name="password" use:validators={[required]} />
-    <Hint for="password" on="required">This is a mandatory field</Hint>
+            <input type="email" name="email" use:validators={[required, email]} />
+            <HintGroup for="email">
+                <Hint on="required">This is a mandatory field</Hint>
+                <Hint on="email" hideWhenRequired>Email is not valid</Hint>
+            </HintGroup>
 
-    <button disabled={!$form.valid}>Login</button>
-</form>
+            <input type="password" name="password" use:validators={[required]} />
+            <Hint for="password" on="required">This is a mandatory field</Hint>
+
+            <button disabled={!$form.valid}>Login</button>
+        </form>
+    </main>
+</body>
 <pre>
 {JSON.stringify($form, null, " ")}
 </pre>
