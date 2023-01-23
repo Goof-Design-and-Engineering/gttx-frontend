4d59b43f49cd1226355b161afbe07cbaf39bb685
KevOub <kjo018@latech.edu>
2023-01-23 13:28:21 -0600

    cool role based code

---
+++ src/lib/Fac.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { currentRole, currentUser, pb } from './pocketbase';
+	import 'bootstrap-icons/font/bootstrap-icons.css';
+	// import { redirect } from '@sveltejs/kit';
+	
+	// function gotoLogin() {
+	// 	throw redirect(302,'/login');
+	// }
+
+</script>
+
+{#if $currentRole != 'facilitator'}
+	<p>UNAUTHORIZED</p>
+	
+{:else}
+	<p>Hello, dave</p>
+{/if}
--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -5,7 +5,7 @@ import { writable } from 'svelte/store';
 export const pb = new Pocketbase("https://api.gttx.app");
 
 export const currentUser = writable(pb.authStore.model);
-
+export const currentRole = writable(pb.authStore.model?.role);
 
 pb.authStore.onChange((auth) => {
     console.log("authstore changed", auth);
