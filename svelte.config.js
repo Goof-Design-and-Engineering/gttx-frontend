0117a700378da7ff52c73f052f70414a67507595
KevOub <kjo018@latech.edu>
2023-01-24 16:39:23 -0600

    add compile static to package

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-	}
+		adapter: adapter()
+	}, paths: {
+		base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+	},
 };
 
 export default config;
