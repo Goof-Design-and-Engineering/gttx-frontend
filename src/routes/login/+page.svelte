d8fb63933108e159de24621436bbe80562797ef4
KevOub <kjo018@latech.edu>
2023-02-07 10:38:44 -0600

    fixed bootstrap being dumb

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,12 +6,6 @@
 	const form = useForm();
 </script>
 
-<head>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/pocketbase/0.10.0/pocketbase.es.mjs"></script>
-	<title>Hello, world!</title>
-</head>
 
 <body>
 	<Login />
