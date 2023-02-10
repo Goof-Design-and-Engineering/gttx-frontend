43713c7ed5f11f8ef45733d674f825c7e34bbe74
KevOub <kjo018@latech.edu>
2023-02-10 14:53:02 -0600

    deleted redundant code

--- src/routes/register/+page.server.js
+++
@@ -1,9 +0,0 @@
-// import { redirect } from '@sveltejs/kit';
-// // export const prerender = true;
-
-// /** @type {import('./$types').Actions} */
-// export const actions = {
-//     login: async (event) => {
-//         throw redirect(302, '/login');
-//     }
-// };
