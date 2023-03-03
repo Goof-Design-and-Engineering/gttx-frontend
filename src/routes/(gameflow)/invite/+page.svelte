eb76ff2902866590b5b1a1dfbf2d4cff6d9c3e77
KevOub <kjo018@latech.edu>
2023-03-02 19:49:31 -0600

    manage org page added as invite page

--- src/routes/(gameflow)/invite/+page.svelte
+++ src/routes/(gameflow)/invite/+page.svelte
@@ -1,38 +1,132 @@
 <script>
-	import { pb, currentUser, currentOrganization } from '$lib/pocketbase';
-	import { onMount } from 'svelte';
+	import { pb, currentUser } from '$lib/pocketbase';
+	import { onMount, onDestroy } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	let organizationEvent;
+	let organizationName;
+	let members = [];
+	let rolechangeErr = '';
 
 	onMount(async () => {
-		// console.log(pb.authStore.model?.org.id);
+		if (currentUser?.org == '') {
+			goto('createorg');
+			return;
+		}
+
+		// get current members
+		const result = await pb
+			.collection('organization')
+			.getOne($currentUser?.org, { expand: 'members' });
+		// merge member list
+		members = [...members, ...result.expand.members];
+
+		// members = await Promise.all(
+		// 	[...members, ...result.members].map(async (item) => {
+		// 		console.log(item);
+		// 		return await pb.collection('users').getOne(item);
+		// 	})
+		// );
+		// console.log(result.members);
+
+		// store organization name
+		organizationName = result.name;
+
+		// subscribe to *all* changes made to the current organization
+		organizationEvent = await pb
+			.collection('organization')
+			.subscribe($currentUser?.org, async function (e) {
+				console.log(e.record);
+				// on change get the members expanded list
+				const result = await pb
+					.collection('organization')
+					.getOne(e.record.id, { expand: 'members' });
+
+				// and set it to the global members
+				members = result.expand.members;
+
+				// messages.push(e.record);
+				// const result = e.members;
+				// members.push(map());
+				// members = await Promise.all(
+				// 	[...members, ...e.members].map(async (item) => {
+				// 		console.log(item);
+				// 		return await pb.collection('users').getOne(item);
+				// 	})
+				// );
+			});
+		// organizationTest = await pb.collection('organization').getOne($currentUser?.org,{});
 	});
 
-	async function usersOfOrg() {
-		const userModel = await pb
-			.collection('users')
-			.getOne(pb.authStore.model?.id, { expand: 'org' });
-		console.log(userModel);
+	// Unsubscribe from realtime messages
+	onDestroy(() => {
+		organizationEvent?.();
+	});
 
-		const organizationRecord = await pb
-			.collection('organization')
-			.getOne(userModel.org);
-		console.log(organizationRecord);
-		return organizationRecord;
-
+	async function deleteUser(userid) {
+		// const result = pb.collection('organization').getOne($currentUser?.org);
+		const result = members.filter((m) => m.id !== userid);
+		console.log(result);
+		// gross javascript that updates the database to point not to the object member but the member ID
+		pb.collection('organization').update($currentUser?.org, {
+			members: result.map((e) => {
+				return e.id;
+			})
+		});
 	}
 
+	async function changeRole(id, role) {
+		// TODO sanitize update here
+		console.log(role);
+		console.log(id);
+		// first update the user
+		const data = { role: role };
+		const result = await pb.collection('users').update(id, data);
+		rolechangeErr = result;
+
+		// then update the organization members list
+		const result2 = await pb
+			.collection('organization')
+			.getOne($currentUser?.org, { expand: 'members' });
+		members = result2.expand.members;
+	}
 </script>
 
-<h1>start</h1>
+{#if rolechangeErr != ''}
+	{rolechangeErr}
+{/if}
 
-{#await usersOfOrg() then record}
-	<article>
-		<h3>
-			{record.name}
-		</h3>
-		{#each record.members as user}
-			<li>
-				{user}
-			</li>
-		{/each}
-	</article>
-{/await}
+<h1>start: {organizationName}</h1>
+
+<br />
+{#each members as member, index (member.id)}
+	<div class="grid">
+		<div>
+			<h3>
+				{#if member.id == $currentUser.id}
+					{member.username} (self)
+				{:else}
+					{member.username}
+				{/if}
+			</h3>
+		</div>
+		<div>
+			{member.role}
+		</div>
+		<div>
+			<details role="list">
+				<summary aria-haspopup="listbox" role="button" class="secondary"> role </summary>
+
+				<ul role="listbox">
+					<li><a on:click={changeRole(member.id, 'facilitator')}>facilitator</a></li>
+					<li><a on:click={changeRole(member.id, 'participant')}>participant</a></li>
+					<li><a on:click={changeRole(member.id, 'observer')}>observer</a></li>
+				</ul>
+			</details>
+		</div>
+
+		<div>
+			<a href="#" role="button" on:click={deleteUser(member.id)}> DELETE</a>
+		</div>
+	</div>
+{/each}
