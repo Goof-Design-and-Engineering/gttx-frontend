103bfd027c223a45fddea52a794684f4efa6336b
Zach Hoover <38825744+ZachHoover@users.noreply.github.com>
2023-03-13 17:37:18 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	kit: {
 		adapter: adapter()
 	}, paths: {
-		base: process.env.NODE_ENV === "production" ? "/gttx-frontend" : "",
+		base: process.env.NODE_ENV === "production" ? "/" : "",
 		"$lib": ["src/lib"],
 		"$lib/*": ["src/lib/*"]
 	},
