334bef78e8cd6c845dfa60e7a7a6ea04f0e8d4a4
Josh <joshpersonguy@outlook.com>
2023-03-24 13:18:01 -0500

    added account settings page

--- src/lib/auth/settings/Edit.svelte
+++ src/lib/auth/settings/Edit.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { useForm, validators, HintGroup, Hint, email, required } from 'svelte-use-form';
 	import {
-		currentRole,
 		currentUser,
-		currentOrganization,
-		currentProfilePic,
 		pb
 	} from '$lib/pocketbase';
 	import { goto } from '$app/navigation';
+
+	var new_username = '';
+	var new_emailaddr = '';
 	
 	async function signout() {
 		pb.authStore.clear();
 		goto('/');
 	}
 
-	async function invite() {
-		goto('/invite');
+    async function back() {
+		goto('/account');
 	}
-
-	
 
 	onMount(() => {
 		if (!$currentUser) {
@@ -27,15 +26,50 @@
 	});
 </script>
 
-
-
-
 {#if $currentUser}
-	<img
-		src="https://api.gttx.app/api/files/_pb_users_auth_/{$currentUser.id}/{$currentUser.avatar}?thumb=100x100"
-		alt={$currentUser.username}
-	/>
-	Signed in as {$currentUser.username}, {$currentRole},{$currentUser.org.name},{$currentOrganization},{$currentUser.avatar}
-	<button on:click={signout}> logout</button>
-	<button on:click={invite}> invite users</button>
+    <!-- <b style="font-size: 40px;">Password Change for {$currentUser.username}</b> -->
+	<hgroup>
+		<h1 style="font-size: 40px;">Edit settings for {$currentUser.username}</h1>
+		<h2>Express yourself!</h2>
+	</hgroup>
+	<!-- <article> -->
+		<!-- Signed in as {$currentUser.username}, {$currentRole},{$currentUser.org.name},{$currentOrganization},{$currentUser.avatar} -->
+		<label>
+			Username
+			<!--  -->
+			<input
+				type="text"
+				name="new_username"
+				id="new_username"
+				value={$currentUser.username}
+				use:validators={[required]}
+			/>
+
+			Email Address
+			<!-- bind:value={new_emailaddr} -->
+			<input
+				type="text"
+				name="new_emailaddr"
+				id="new_emailaddr"
+				value={$currentUser.email}
+				use:validators={[required]}
+				disabled
+			/>
+
+			<label for="file">Profile Picture
+				<input 
+					type="file"
+					id="pfp"
+					name="pfp"
+					accept="image/*"
+					value=""
+				>
+			</label>
+		</label>
+	<!-- </article> -->
+    <br />
+    <div class="grid">
+        <button on:click={back} class="secondary"> Back to Settings</button>
+        <button on:click={signout}> Submit</button>
+    </div>
 {/if}
