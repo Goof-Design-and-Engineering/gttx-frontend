56f0aab7979f051cc92a3b42478ba11b2fb79b8a
Nolan Y? <ndymario@gmail.com>
2023-03-25 22:00:18 -0500

    Add change password functionality

--- src/lib/auth/settings/Pass.svelte
+++ src/lib/auth/settings/Pass.svelte
@@ -1,19 +1,39 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { useForm, validators, HintGroup, Hint, email, required } from 'svelte-use-form';
-	import {
-		currentUser,
-		pb
-	} from '$lib/pocketbase';
+	import { currentUser, pb } from '../../pocketbase';
 	import { goto } from '$app/navigation';
 
 	var old_password = '';
 	var new_password = '';
 	var new_password_again = '';
+
+	const form = useForm();
 	
 	async function submit() {
-		alert("Not implemented.");
-		goto('/account');
+		console.log(currentUser)
+		try{
+			await pb.collection('users').authWithPassword($currentUser.username, old_password)
+
+			if((new_password == new_password_again) && (new_password != "")){
+				console.log("Passwords matched, clearing auth and updating password!")
+				try{
+					await pb.collection('users').update($currentUser.id, {oldPassword: old_password, password: new_password, passwordConfirm: new_password_again})
+					pb.authStore.clear();
+					goto("/login")
+				} catch(e){
+					console.log(e)
+					alert("Something went wrong, please try again.")
+				}
+				
+			}
+			else{
+				alert("Your passwords do not match!")
+			}
+		} catch(e){
+			console.log(e)
+			alert("Your old password was not correct!")
+		}
 	}
 
     async function back() {
@@ -33,8 +53,7 @@
 		<h1 style="font-size: 40px;">Password Change for {$currentUser.username}</h1>
 		<h2>Be careful! Password changes are permanent!</h2>
 	</hgroup>
-	<!-- <article> -->
-		<!-- Signed in as {$currentUser.username}, {$currentRole},{$currentUser.org.name},{$currentOrganization},{$currentUser.avatar} -->
+	<form use:form on:submit|preventDefault method="POST">
 		<label>
 			Old Password
 			<input
@@ -66,7 +85,7 @@
 				use:validators={[required]}
 			/>
 		</label>
-	<!-- </article> -->
+	</form>
     <br />
     <div class="grid">
         <button on:click={back} class="secondary"> Back to Settings</button>
