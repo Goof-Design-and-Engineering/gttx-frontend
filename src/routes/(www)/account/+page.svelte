80ff51f279a8a9b2f832c3372b358ea3e12354f2
joshish <joshpersonguy@outlook.com>
2023-03-26 22:17:26 -0500

    lol oops

--- src/routes/(www)/account/+page.svelte
+++ src/routes/(www)/account/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { useForm, validators, HintGroup, Hint, email, required } from 'svelte-use-form';
 	import 'bootstrap-icons/font/bootstrap-icons.css';
-	import '$lib/style/main.css'
+	// import '$lib/style/main.css'
 	import Settings from '$lib/auth/settings/Account.svelte';
 	const form = useForm();
 </script>
