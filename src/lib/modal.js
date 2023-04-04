95895222d55b1acb3716063282b38de184ce4fea
Nolan Y? <ndymario@gmail.com>
2023-04-04 15:29:42 -0500

    Fix file tracking

---
+++ src/lib/modal.js
@@ -0,0 +1,78 @@
+/*
+ * Modal
+ *
+ * Pico.css - https://picocss.com
+ * Copyright 2019-2023 - Licensed under MIT
+ */
+
+// Config
+export const isOpenClass = 'modal-is-open';
+export const openingClass = 'modal-is-opening';
+export const closingClass = 'modal-is-closing';
+export const animationDuration = 400; // ms
+export let visibleModal = null;
+
+
+// Toggle modal
+export const toggleModal = event => {
+  const modal = document.getElementById(event.currentTarget.getAttribute('data-target'));
+  (typeof(modal) != 'undefined' && modal != null)
+    && isModalOpen(modal) ? closeModal(modal) : openModal(modal)
+}
+
+// Is modal open
+export const isModalOpen = modal => {
+  return modal.hasAttribute('open') && modal.getAttribute('open') != 'false' ? true : false;
+}
+
+// Open modal
+export const openModal = modal => {
+  if (isScrollbarVisible()) {
+    document.documentElement.style.setProperty('--scrollbar-width', `${getScrollbarWidth()}px`);
+  }
+  document.documentElement.classList.add(isOpenClass, openingClass);
+  setTimeout(() => {
+    visibleModal = modal;
+    document.documentElement.classList.remove(openingClass);
+  }, animationDuration);
+  modal.setAttribute('open', true);
+}
+
+// Close modal
+export const closeModal = modal => {
+  visibleModal = null;
+  document.documentElement.classList.add(closingClass);
+  setTimeout(() => {
+    document.documentElement.classList.remove(closingClass, isOpenClass);
+    document.documentElement.style.removeProperty('--scrollbar-width');
+    modal.removeAttribute('open');
+  }, animationDuration);
+}
+
+// Get scrollbar width
+export const getScrollbarWidth = () => {
+
+  // Creating invisible container
+  const outer = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+  outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+  document.body.appendChild(outer);
+
+  // Creating inner element and placing it in the container
+  const inner = document.createElement('div');
+  outer.appendChild(inner);
+
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
+}
+
+// Is scrollbar visible
+const isScrollbarVisible = () => {
+  return document.body.scrollHeight > screen.height;
+}
