aeb69f7f5824ff4ab18d413461e9703b2f733aa7
KevOub <kjo018@latech.edu>
2023-04-24 08:53:50 -0500

    create redact folder of folders we do not need

---
+++ src/routes/(redact)/(roles)/p/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { currentRole } from '$lib/pocketbase';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	onMount(() => {
+		if ($currentRole == 'observer' || $currentRole == "facilitator") {
+			goto('/o/dashboard');
+		}else{
+            goto('/account')
+        }
+	});
+</script>
