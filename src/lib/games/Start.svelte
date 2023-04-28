cb4b1277dd27e43b3d44f8ec614982ba52ebecba
Nolan Y? <ndymario@gmail.com>
2023-04-28 16:18:02 -0500

    No more exploding please Start Page

--- src/lib/games/Start.svelte
+++ src/lib/games/Start.svelte
@@ -17,6 +17,13 @@
 	import Switch from '$lib/util/Switch.svelte';
 
 	import Carousel from 'svelte-carousel';
+	let currentPage = 0;
+	let currScenario;
+	let showScenarioToggle = false;
+
+	function showScenario() {
+		showScenarioToggle = !showScenarioToggle;
+	}
 
 	import Modal from '$lib/Modal.svelte';
 	import AddEmail from '../util/AddEmail.svelte';
@@ -34,6 +41,11 @@
 
 	var inputs = [];
 
+	function setPage(number) {
+		currentPage = number;
+		console.log(number);
+	}
+
 	async function submitinvitecode() {
 		try {
 			isFailure = false;
@@ -49,6 +61,12 @@
 		const resultList = await pb.collection('scenario').getList(1, 50);
 		console.log(resultList);
 		return resultList.items;
+	}
+
+	function fetchScenario(scenario) {
+		console.log(scenario);
+		showScenario = true;
+		return scenario;
 	}
 
 	function setModal(scenario, module) {
@@ -99,51 +117,52 @@
 			</header>
 			{#await getScenarios()}
 				<progress />
-			{:then rawScenarios}
-				{#await formatScenario(rawScenarios)}
-					<progress />
-				{:then scenarios}
-					<Carousel>
-						{#each scenarios as scenario}
-							<details>
-								<summary>{scenario.name}</summary>
-								<h2>{scenario.contents?.overview?.name || ''}</h2>
-								<h5>{scenario.contents?.name || ''} {scenario.contents?.source || ''}</h5>
-								<hr />
-								<h4>Purpose</h4>
-								<p>{scenario.contents?.overview?.purpose || ''}</p>
-								<h4>Scope</h4>
-								<p>{scenario.contents?.overview?.scope || ''}</p>
-								<hr />
-								<h4>Objectives</h4>
-								<ul>
-									{#each scenario.contents?.overview.objectives || [] as objective}
-										<li>
-											{objective}
-										</li>
-									{/each}
-								</ul>
-								<hr />
-								<h4>Modules</h4>
-								<ul>
-									{#each Object.entries(scenario.contents?.modules || []) as [name, module]}
-										<em>
-											{name}
-										</em>
-										<p>{module.description}</p>
-										<button on:click={() => setModal(scenario.id, name)}>
-											Choose this scenario and module</button
-										>
-									{/each}
-								</ul>
-							</details>
-						{/each}
-					</Carousel>
-				{:catch error}
-					{error}
-				{/await}
+			{:then scenarios}
+				<Carousel on:pageChange={(event) => setPage(event.detail)}>
+					{#each scenarios as _}
+						<details>
+							<summary on:click={showScenario}>{scenarios[currentPage].name}</summary>
+							{#if showScenarioToggle}
+								{#await fetchScenario(scenarios[currentPage])}
+									<progress />
+								{:then scenario}
+									<h2>{scenario.contents?.overview?.name || ''}</h2>
+									<h5>{scenario.contents?.name || ''} {scenario.contents?.source || ''}</h5>
+									<hr />
+									<h4>Purpose</h4>
+									<p>{scenario.contents?.overview?.purpose || ''}</p>
+									<h4>Scope</h4>
+									<p>{scenario.contents?.overview?.scope || ''}</p>
+									<hr />
+									<h4>Objectives</h4>
+									<ul>
+										{#each scenario.contents?.overview.objectives || [] as objective}
+											<li>
+												{objective}
+											</li>
+										{/each}
+									</ul>
+									<hr />
+									<h4>Modules</h4>
+									<ul>
+										{#each Object.entries(scenario.contents?.modules || []) as [name, module]}
+											<em>
+												{name}
+											</em>
+											<p>{module.description}</p>
+											<button on:click={() => setModal(scenario.id, name)}>
+												Choose this scenario and module</button
+											>
+										{/each}
+									</ul>
+								{:catch error}
+									{error}
+								{/await}
+							{/if}
+						</details>
+					{/each}
+				</Carousel>
 			{:catch error}
-				<!-- promise was rejected -->
 				{error}
 			{/await}
 		</article>
